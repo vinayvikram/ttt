27969c961762aecfda91f4e1f5aa060c07a16cd1
Vinay Vikram <vinayandvikram@gmail.com>
2021-12-19 18:06:09 +0530

    new

--- src/index.js
+++ src/index.js
@@ -2,15 +2,12 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import StartPage from "./routes/startPage";
 import GamePage from "./routes/gamePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+//import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<StartPage />} />
-      <Route path="/play/0" element={<GamePage mode={0} />} />
-      <Route path="/play/1" element={<GamePage mode={1} />} />
-    </Routes>
-  </BrowserRouter>,
+  <div>
+    <StartPage />
+    <GamePage mode={0} />
+  </div>,
   document.getElementById("root")
 );
