63547daaed6c7bdd311f8ac485260ea19a92826b
Vinay Vikram <vinayandvikram@gmail.com>
2021-12-19 19:09:53 +0530

    re-set

--- src/index.js
+++ src/index.js
@@ -2,12 +2,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import StartPage from "./routes/startPage";
 import GamePage from "./routes/gamePage";
-//import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <div>
-    <StartPage />
-    <GamePage mode={0} />
-  </div>,
+  <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<StartPage />} />
+      <Route path="/play/0" element={<GamePage mode={0} />} />
+      <Route path="/play/1" element={<GamePage mode={1} />} />
+    </Routes>
+  </BrowserRouter>,
   document.getElementById("root")
 );
