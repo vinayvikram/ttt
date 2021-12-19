169dd803ff3a87af817cd1b559cd4fce2a06ab1c
Vinay Vikram <vinayandvikram@gmail.com>
2021-12-19 19:19:10 +0530

    reset

--- src/index.js
+++ src/index.js
@@ -2,15 +2,15 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import StartPage from "./routes/startPage";
 import GamePage from "./routes/gamePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { HashRouter, Routes, Route } from "react-router-dom";
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<StartPage />} />
       <Route path="/play/0" element={<GamePage mode={0} />} />
       <Route path="/play/1" element={<GamePage mode={1} />} />
     </Routes>
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById("root")
 );
