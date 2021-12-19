0cb18a755ea03631999d9be85aeec5b51516486d
Vinay Vikram <vinayandvikram@gmail.com>
2021-12-19 19:24:36 +0530

    nexagain

--- src/index.js
+++ src/index.js
@@ -7,9 +7,9 @@ import { HashRouter, Routes, Route } from "react-router-dom";
 ReactDOM.render(
   <HashRouter>
     <Routes>
-      <Route path="/" element={<StartPage />} />
-      <Route path="/play/0" element={<GamePage mode={0} />} />
-      <Route path="/play/1" element={<GamePage mode={1} />} />
+      <Route path="/ttt/" element={<StartPage />} />
+      <Route path="/ttt/play/0" element={<GamePage mode={0} />} />
+      <Route path="/ttt/play/1" element={<GamePage mode={1} />} />
     </Routes>
   </HashRouter>,
   document.getElementById("root")
